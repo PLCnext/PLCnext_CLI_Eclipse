bb2a0867d11765c1814a4c4aae41a24a057c1996
pxcsw-afbuild <67624542+pxcsw-afbuild@users.noreply.github.com>
2019-07-22 08:54:24 +0200

    improve usability in SdksPreferencePage #exported-commit:75591601c0dbe743454a25fb11ffdc74ab255f88

--- src/com.phoenixcontact.plcnext.common/src/main/java/com/phoenixcontact/plcnext/common/preferences/SdksPreferencePage.java
+++ src/com.phoenixcontact.plcnext.common/src/main/java/com/phoenixcontact/plcnext/common/preferences/SdksPreferencePage.java
@@ -64,7 +64,7 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 {
 	private SdkPreferenceDataModel model = new SdkPreferenceDataModel();
 	private ListViewer sdkViewer;
-	private ICommandManager commandManager; 
+	private ICommandManager commandManager;
 
 	/**
 	 * @see org.eclipse.jface.preference.PreferencePage#Constructor
@@ -76,7 +76,7 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 		IDIHost host = ContextInjectionFactory.make(IDIHost.class, context);
 		commandManager = host.getExport(ICommandManager.class);
 	}
-	
+
 	@Override
 	public void init(IWorkbench workbench)
 	{
@@ -88,15 +88,14 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 		Composite control = new Composite(parent, SWT.NONE);
 		control.setLayout(new GridLayout(2, false));
 		noDefaultAndApplyButton();
-		
-		Label description = new Label(control, SWT.NONE); 
+
+		Label description = new Label(control, SWT.NONE);
 		description.setLayoutData(new GridData(SWT.BEGINNING, SWT.TOP, false, false, 2, 1));
 		description.setText(Messages.SdksPreferencePage_SDKsLabel);
-		
-		
+
 		sdkViewer = new ListViewer(control, SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
 		sdkViewer.getControl().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 3));
-		
+
 		sdkViewer.setLabelProvider(new LabelProvider());
 		sdkViewer.setComparator(new ViewerComparator());
 		sdkViewer.setContentProvider(new IStructuredContentProvider()
@@ -129,9 +128,7 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 			}
 		});
 		fillViewer();
-		
-		
-		
+
 		Button addButton = new Button(control, SWT.PUSH);
 		GridData addButtonGD = new GridData();
 		addButtonGD.widthHint = convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
@@ -139,7 +136,7 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 		addButton.setText(Messages.SdksPreferencePage_AddButton);
 		addButton.addListener(SWT.Selection, event -> handleAddButtonSelected());
 		addButton.setToolTipText(Messages.SdksPreferencePage_ToolTipAddButton);
-		
+
 		Button installButton = new Button(control, SWT.PUSH);
 		GridData installButtonGD = new GridData(SWT.DEFAULT, SWT.DEFAULT, false, false);
 		installButtonGD.widthHint = convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
@@ -147,8 +144,7 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 		installButton.setText(Messages.SdksPreferencePage_InstallButton);
 		installButton.addListener(SWT.Selection, event -> handleInstallButtonSelected());
 		installButton.setToolTipText(Messages.SdksPreferencePage_ToolTipInstallButton);
-		
-		
+
 		Button removeButton = new Button(control, SWT.PUSH);
 		GridData removeButtonGD = new GridData(SWT.DEFAULT, SWT.DEFAULT, false, true);
 		removeButtonGD.widthHint = convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
@@ -156,26 +152,26 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 		removeButton.setText(Messages.SdksPreferencePage_RemoveButton);
 		removeButton.addListener(SWT.Selection, event -> handleRemoveButtonSelected());
 		removeButton.setToolTipText(Messages.SdksPreferencePage_ToolTipRemoveButton);
-		
+
 		sdkViewer.addSelectionChangedListener(new ISelectionChangedListener()
 		{
-		
+
 			@Override
 			public void selectionChanged(SelectionChangedEvent event)
 			{
-				if(event.getSelection() instanceof IStructuredSelection) 
+				if (event.getSelection() instanceof IStructuredSelection)
 				{
-				
+
 					IStructuredSelection selection = (IStructuredSelection) event.getSelection();
-		
+
 					removeButton.setEnabled(selection.size() > 0);
 				}
 			}
 		});
 		sdkViewer.setSelection(StructuredSelection.EMPTY);
-		
+
 		setVisible(true);
-		
+
 		return control;
 	}
 
@@ -183,58 +179,60 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 	{
 		AddSdkDialog dialog = new AddSdkDialog(getShell());
 		String result = dialog.openWithResult();
-		
-		if(result != null)
+
+		if (result != null)
 		{
 			model.addSdkForSet(result);
-			
+
 			sdkViewer.add(result);
 		}
 	}
-	
+
 	private void handleInstallButtonSelected()
 	{
 		InstallSdkDialog dialog = new InstallSdkDialog(getShell());
 		InstallSdkDialogResult result = dialog.openWithResult();
-		
-		if(result != null)
+
+		if (result != null)
 		{
 			model.addSdkForInstall(result.getArchive(), result.getDestination(), result.getForce());
 			sdkViewer.add(result.getDestination());
 		}
 	}
-	
+
 	private void handleRemoveButtonSelected()
 	{
-		
-		
+
 		int index = sdkViewer.getList().getSelectionIndex();
-		if(index >= 0)
+		if (index >= 0)
 		{
 			String element = sdkViewer.getList().getItem(index);
-			
+
 			sdkViewer.getList().remove(element);
 			model.removeSdk(element);
-		sdkViewer.setSelection(StructuredSelection.EMPTY);
+			sdkViewer.setSelection(StructuredSelection.EMPTY);
+		}
+
 	}
-	
-	}
-	
+
 	private void fillViewer()
 	{
-		try {
-			CommandResult commandResult = commandManager.executeCommand(commandManager.createCommand(null, GetSdksCommand.class), false, null);
+		try
+		{
+			CommandResult commandResult = commandManager
+					.executeCommand(commandManager.createCommand(null, GetSdksCommand.class), false, null);
 			GetSdksCommandResult sdksCommandResult = commandResult.convertToGetSdksCommandResult();
-			
+
 			Sdk[] sdks = sdksCommandResult.getSdks();
-			
+
 			for (Sdk sdk : sdks)
 			{
 				sdkViewer.add(sdk.getPath());
 			}
-		}catch(ProcessExitedWithErrorException e)
+		} catch (ProcessExitedWithErrorException e)
 		{
-			ErrorDialog.openError(getShell(), null, null, new Status(Status.ERROR, Activator.PLUGIN_ID, Messages.SdksPreferencePage_GetSdksError, e));
+			ErrorDialog.openError(getShell(), null, null,
+					new Status(Status.ERROR, Activator.PLUGIN_ID, Messages.SdksPreferencePage_GetSdksError, e));
 		}
 	}
 	
@@ -244,99 +242,122 @@ public class SdksPreferencePage extends PreferencePage implements IWorkbenchPref
 		List<String> removeSdks = model.getRemoveSdks();
 		List<String> setSdks = model.getSetSdks();
 		List<InstallSdk> installSdks = model.getInstallSdks();
-		
-		if(removeSdks.size() > 0) 
+
+		boolean remove = false;
+
+		if (removeSdks.size() > 0)
 		{
 			RemoveSdkDialog dialog = new RemoveSdkDialog(null, removeSdks);
 			int result = dialog.open();
-			if(result != Window.OK) {
+			if (result != Window.OK)
+			{
 				return false;
 			}
-			if(dialog.getButtonSelection())
-			{
-				for(String sdk : removeSdks)
-				{
-		
-					IFileStore fileStore = EFS.getLocalFileSystem().getStore(new Path(sdk));
-					if (fileStore.fetchInfo().isDirectory() && fileStore.fetchInfo().exists())
-					{
-						try
-						{
-							fileStore.delete(EFS.NONE, null);
-						} catch (CoreException e)
-						{
-							Activator.getDefault().logError("Could not delete sdk directory", e);
-						}
-					}
-				}
-			}
+			remove = dialog.getButtonSelection();
 		}
-		
-		Job okJob = new Job(Messages.SdksPreferencePage_SdksJobName)
+
+		class OKJob extends Job
 		{
+			boolean remove;
+			public OKJob(boolean remove)
+			{
+				super(Messages.SdksPreferencePage_SdksJobName);
+				this.remove = remove;
+			}
 			
 			@Override
 			public IStatus run(IProgressMonitor monitor)
 			{
-				int sum = 1 + removeSdks.size() + setSdks.size() + installSdks.size() * 5;
+				int r = remove ? removeSdks.size() : 0;
+				int sum = 1 + r + removeSdks.size() + setSdks.size() + installSdks.size() * 5;
 				SubMonitor subMonitor = SubMonitor.convert(monitor, sum);
-				subMonitor.split(1);
+				subMonitor.worked(1);
 				
+				
+				if (remove)
+				{
+					for (String sdk : removeSdks)
+					{
+						subMonitor.setTaskName("Delete "+ sdk);
+						IFileStore fileStore = EFS.getLocalFileSystem().getStore(new Path(sdk));
+						if (fileStore.fetchInfo().isDirectory() && fileStore.fetchInfo().exists())
+						{
+							try
+							{
+								fileStore.delete(EFS.NONE, null);
+							} catch (CoreException e)
+							{
+								Activator.getDefault().logError("Could not delete sdk directory", e);
+							}
+						}
+						subMonitor.worked(1);
+					}
+				}
+
 				for (String sdk : removeSdks)
 				{
-					subMonitor.split(1);
+					subMonitor.setTaskName("Remove "+ sdk + " from settings");
 					Map<String, String> options = new HashMap<>();
 					options.put(SetSettingCommand.OPTION_SdkPaths, sdk);
 					options.put(SetSettingCommand.OPTION_REMOVE, null);
 					try
 					{
-						commandManager.executeCommand(commandManager.createCommand(options, SetSettingCommand.class), false, monitor);
+						commandManager.executeCommand(commandManager.createCommand(options, SetSettingCommand.class),
+								false, monitor);
 					} catch (ProcessExitedWithErrorException e)
 					{
-						return new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not remove sdk "+sdk, e); //$NON-NLS-1$
+						return new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not remove sdk " + sdk, e); //$NON-NLS-1$
 					}
+					subMonitor.worked(1);
 				}
-				
-				for(String sdk : setSdks)
+
+				for (String sdk : setSdks)
 				{
-					subMonitor.split(1);
+					subMonitor.setTaskName("Add "+ sdk + " to settings");
 					Map<String, String> options = new HashMap<>();
 					options.put(SetSettingCommand.OPTION_ADD, null);
 					options.put(SetSettingCommand.OPTION_SdkPaths, sdk);
 					try
 					{
-						commandManager.executeCommand(commandManager.createCommand(options, SetSettingCommand.class), false, monitor);
+						commandManager.executeCommand(commandManager.createCommand(options, SetSettingCommand.class),
+								false, monitor);
 					} catch (ProcessExitedWithErrorException e)
 					{
-						return new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not set sdk "+sdk, e); //$NON-NLS-1$
+						return new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not set sdk " + sdk, e); //$NON-NLS-1$
 					}
+					subMonitor.worked(1);
 				}
-				
-				
-				for(InstallSdk sdk : installSdks)
+
+				for (InstallSdk sdk : installSdks)
 				{
-					subMonitor.split(5);
+					subMonitor.setTaskName("Install sdk "+ sdk.getArchive());
 					Map<String, String> options = new HashMap<>();
 					options.put(InstallSdkCommand.OPTION_PATH, sdk.getArchive());
 					options.put(InstallSdkCommand.OPTION_DESTINATION, sdk.getDestination());
-					if(sdk.getForce())
+					if (sdk.getForce())
 						options.put(InstallSdkCommand.OPTION_FORCE, null);
-					
+
 					try
 					{
-						commandManager.executeCommand(commandManager.createCommand(options, InstallSdkCommand.class), monitor);
+						commandManager.executeCommand(commandManager.createCommand(options, InstallSdkCommand.class),
+								monitor);
 					} catch (ProcessExitedWithErrorException e)
 					{
-						return new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not install sdk "+sdk.getArchive(), e); //$NON-NLS-1$
+						return new Status(Status.ERROR, Activator.PLUGIN_ID,
+								"Could not install sdk " + sdk.getArchive(), e); //$NON-NLS-1$
 					}
-					
+					subMonitor.worked(5);
+
 				}
+				subMonitor.worked(1);
+				subMonitor.done();
 				return Status.OK_STATUS;
 			}
-		};
-		
+		}
+		Job okJob = new OKJob(remove);
+
 		okJob.schedule();
-		
+
 		return super.performOk();
 	}
 
