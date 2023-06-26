c581d1d3052c8f0cdd75b1c5e2833c57376128de
pxcsw-afbuild <67624542+pxcsw-afbuild@users.noreply.github.com>
2023-06-26 15:11:58 +0200

    activate iec datatype selection in port comment dialog #exported-commit:7ca11a2bcfff3b5e9270a41fd98183250eebea98

--- src/com.phoenixcontact.plcnext.cplusplus.project/src/main/java/com/phoenixcontact/plcnext/cplusplus/project/comfortfunction/SelectPortAttributesWizardPage.java
+++ src/com.phoenixcontact.plcnext.cplusplus.project/src/main/java/com/phoenixcontact/plcnext/cplusplus/project/comfortfunction/SelectPortAttributesWizardPage.java
@@ -172,10 +172,6 @@ public class SelectPortAttributesWizardPage extends WizardPage
 		// ********************separator******************
 		Label separatorH2 = new Label(container, SWT.SEPARATOR | SWT.HORIZONTAL | SWT.SHADOW_OUT);
 		GridData sep2Data = new GridData(SWT.FILL, SWT.CENTER, false, true, 3, 1);
-		
-		sep2Data.exclude = true;
-		separatorH2.setVisible(false);
-		
 		separatorH2.setLayoutData(sep2Data);
 		// *****************IEC datatype*******************
 
@@ -183,10 +179,6 @@ public class SelectPortAttributesWizardPage extends WizardPage
 		datatypeGroup.setText("IEC datatype mapping");
 		datatypeGroup.setLayout(new GridLayout(2, false));
 		GridData groupData = new GridData(SWT.BEGINNING, SWT.CENTER, false, false, 3, 1);
-		
-		groupData.exclude = true;
-		datatypeGroup.setVisible(false);
-		
 		datatypeGroup.setLayoutData(groupData);
 		
 		
